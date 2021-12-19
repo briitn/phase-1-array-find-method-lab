f48b60073408aedcb1ca686a555324bcc2fd58c0
princegobah <babynamek@gmail.com>
2021-12-18 21:11:39 -0500

    commit tree a638896d1a58895bdbcceb98a9406caec3758254

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+
+const record =[
+  { year: "2015",result: "W"},
+  { year: "2014", result: "W"},
+  { year: "2013", result: "L"},]
+  //...
+  //function superbowlWin(record){if (record.result==='W')return record.year}
+  function superbowlWin(record){for (let obj of record)if (obj.result==="W"){return obj.year}}//else return undefined}
+ console.log((superbowlWin(record)))
+ //console.log(record[0].result)
